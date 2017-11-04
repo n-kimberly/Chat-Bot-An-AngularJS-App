4c863c17ec7547f8ec7f01cc39d43a3d5f438afc
n-kimberly <kimberlyvnguyen@gmail.com>
2017-11-04 12:03:22 -0700

    Convert Bloc Chat to Unclever Bots
    * Adjust titles
    * Source Faker.js to generate bot responses
    * Clear message form after bot responds

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -8,7 +8,6 @@ var home;
     // Inject cookies object to retrieve associated username
     function HomeCtrl(Room, Message, $uibModal, $cookies) {
         home = this;
-
         home.rooms = Room.all;
         home.activeRoom = null;
 
@@ -26,14 +25,25 @@ var home;
         home.setActiveRoom = function (room) {
             home.activeRoom = room;
             home.messages = Message.getByRoomId(home.activeRoom.$id);
-        }
+        };
 
         home.sendMessage = function () {
+          // User message
             home.newMessage.roomId = home.activeRoom.$id;
             home.newMessage.username = home.currentUser;
             home.newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
             Message.send(home.newMessage);
-        }
+        };
+
+        home.respond = function () {
+          // Bot auto response
+            home.newMessage.roomId = home.activeRoom.$id;
+            home.newMessage.username = "The Un-Clever Bot";
+            home.newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
+            home.newMessage.content = faker.company.bs() // Faker data here?
+            Message.send(home.newMessage);
+            home.newMessage.content = null;
+        };
     }
 
     angular
